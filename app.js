ac0080106d6fdd4c5919226f8b624350cb011dcc
Rizky Irmawan <rzkyirmwn@gmail.com>
2019-10-11 09:44:50 +0700

    Implementing .env variables

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.json());
 app.use(morgan('dev'));
+app.use(express.json());
 app.use(express.static(path.join(`${__dirname}/public`)));
 
 app.use((req, res, next) => {
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+const dotenv = require('dotenv');
 const app = require('./app');
+
+dotenv.config({ path: './.env' });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Up and running on port ${port}...`));
