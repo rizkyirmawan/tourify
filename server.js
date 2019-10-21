22f94b1a9e47aae6d91f6c3ad1457f83e3ce98e9
Rizky Irmawan <rzkyirmwn@gmail.com>
2019-10-21 10:53:46 +0700

    Added Mongoose and connect to local DB

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './.env' });
+
+mongoose
+	.connect(process.env.DATABASE_LOCAL, {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false
+	})
+	.then(() => console.log('MongoDB connected!'))
+	.catch(err => console.log(`Error: ${err}`));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Up and running on port ${port}...`));
