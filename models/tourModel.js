d3246b6d3af4892363bebc48516a785090e6f092
Rizky Irmawan <rzkyirmwn@gmail.com>
2019-10-27 10:29:07 +0700

    Added some more validation

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,96 +1,104 @@
-const mongoose = require('mongoose');
-const slugify = require('slugify');
+const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const tourSchema = mongoose.Schema(
-	{
-		name: {
-			type: String,
-			required: [true, 'A tour must have a name'],
-			unique: true,
-			trim: true
-		},
-		duration: {
-			type: Number,
-			required: [true, 'A tour must have a duration']
-		},
-		difficulty: {
-			type: String,
-			required: [true, 'A tour must have a difficulty']
-		},
-		maxGroupSize: {
-			type: Number,
-			required: [true, 'A tour must have a maximum group size']
-		},
-		description: {
-			type: String,
-			trim: true,
-			required: [true, 'A tour must have a description']
-		},
-		summary: {
-			type: String,
-			trim: true,
-			required: [true, 'A summary of a tour must be specified']
-		},
-		ratingsAverage: {
-			type: Number,
-			default: 4.0
-		},
-		ratingsQuantity: {
-			type: Number,
-			default: 0
-		},
-		price: {
-			type: Number,
-			required: [true, 'Price must be specified']
-		},
-		priceDiscount: Number,
-		imageCover: {
-			type: String,
-			required: [true, 'A tour must have a cover image']
-		},
-		images: [String],
-		createdAt: {
-			type: Date,
-			default: Date.now(),
-			select: false
-		},
-		startDates: [Date],
-		slug: String,
-		secretTour: {
-			type: Boolean,
-			default: false
-		}
-	},
-	{
-		toJSON: { virtuals: true },
-		toObject: { virtuals: true }
-	}
+  {
+    name: {
+      type: String,
+      required: [true, "A tour must have a name"],
+      unique: true,
+      trim: true,
+      minlength: [10, "A tour must have at least 10 characters"],
+      maxlength: [50, "A tour must be less or equal than 50 characters"]
+    },
+    duration: {
+      type: Number,
+      required: [true, "A tour must have a duration"]
+    },
+    difficulty: {
+      type: String,
+      required: [true, "A tour must have a difficulty"],
+      enum: {
+        values: ["easy", "medium", "difficult"],
+        message: "Difficulty is either: easy, medium or difficult"
+      }
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, "A tour must have a maximum group size"]
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: [true, "A tour must have a description"]
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, "A summary of a tour must be specified"]
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, "Ratings must be above or equal than 1.0"],
+      max: [5, "Ratings must be less or equal than 5.0"]
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0
+    },
+    price: {
+      type: Number,
+      required: [true, "Price must be specified"]
+    },
+    priceDiscount: Number,
+    imageCover: {
+      type: String,
+      required: [true, "A tour must have a cover image"]
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false
+    },
+    startDates: [Date],
+    slug: String,
+    secretTour: {
+      type: Boolean,
+      default: false
+    }
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
 );
 
 // Virtual Properties
-tourSchema.virtual('durationInWeeks').get(function() {
-	const week = this.duration / 7;
-	return week.toPrecision(2);
+tourSchema.virtual("durationInWeeks").get(function() {
+  const week = this.duration / 7;
+  return week.toPrecision(2);
 });
 
 // Document Middleware (Case: Slug, Method: .save() and .create())
-tourSchema.pre('save', function(next) {
-	this.slug = slugify(this.name, { lower: true });
-	next();
+tourSchema.pre("save", function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 // Query Middleware (Case: Secret Tour, Method: find(), findOne(), etc.)
 tourSchema.pre(/^find/, function(next) {
-	this.find({ secretTour: { $ne: true } });
-	next();
+  this.find({ secretTour: { $ne: true } });
+  next();
 });
 
 // Aggregation Middleware (Case: Hiding Secret Tours in Aggregation Pipeline)
-tourSchema.pre('aggregate', function(next) {
-	this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
-	next();
+tourSchema.pre("aggregate", function(next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  next();
 });
 
-const Tour = mongoose.model('Tour', tourSchema);
+const Tour = mongoose.model("Tour", tourSchema);
 
 module.exports = Tour;
