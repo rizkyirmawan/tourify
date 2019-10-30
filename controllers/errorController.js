dcfb32301384fe78e207e205a76dde93bc047c9e
Rizky Irmawan <rzkyirmwn@gmail.com>
2019-10-30 09:01:43 +0700

    Improved production mode error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,63 @@
+const AppError = require('./../utils/appError');
+
+// Handle Duplicate IDs
+const handleCastError = err => {
+	const message = `Invalid ${err.path}: ${err.value}`;
+	return new AppError(message, 400);
+};
+
+// Handle Duplicate Fields
+const handleDuplicateError = err => {
+	const value = err.errmsg.match(/"([^"]*)"/)[1];
+	const message = `Duplicate field value of: ${value}. Please use another value!`;
+	return new AppError(message, 400);
+};
+
+// Handle Validation Errors
+const handleValidationError = err => {
+	const errors = Object.values(err.errors).map(el => el.message);
+	const message = `Invalid input data: ${errors.join(', ')}.`;
+	return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+	res.status(err.statusCode).json({
+		status: err.status,
+		error: err,
+		message: err.message,
+		stack: err.stack
+	});
+};
+
+const sendErrorProd = (err, res) => {
+	if (err.isOperational) {
+		res.status(err.statusCode).json({
+			status: err.status,
+			message: err.message
+		});
+	} else {
+		console.error('Error: ', err);
+
+		res.status(500).json({
+			status: 'Error',
+			message: 'Something went very wrong!'
+		});
+	}
+};
+
 module.exports = (err, req, res, next) => {
 	err.status = err.status || 'Error';
 	err.statusCode = err.statusCode || 500;
 
-	res.status(err.statusCode).json({
-		status: err.status,
-		message: err.message
-	});
+	if (process.env.NODE_ENV === 'development') {
+		sendErrorDev(err, res);
+	} else if (process.env.NODE_ENV === 'production') {
+		let error = { ...err };
+
+		if (error.name === 'CastError') error = handleCastError(error);
+		if (error.name === 'ValidationError') error = handleValidationError(error);
+		if (error.code === 11000) error = handleDuplicateError(error);
+
+		sendErrorProd(error, res);
+	}
 };
