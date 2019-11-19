9f46e34f3fc0bd43a31888aad20a1ba090a4647f
Rizky Irmawan <rzkyirmwn@gmail.com>
2019-11-19 16:29:11 +0700

    Logging the error

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -29,7 +29,7 @@ const signin = async (email, password) => {
       location.assign('/');
     }
   } catch (err) {
-    console.log(err.response);
+    console.log(err.response.data);
     await Swal.fire({
       icon: 'error',
       title: err.response.data.message,
