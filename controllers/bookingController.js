4d4f7322ddf37a07024a97cad259cc24cc55fd28
Rizky Irmawan <rzkyirmwn@gmail.com>
2019-11-20 10:23:40 +0700

    Modify images on stripe session

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -18,7 +18,9 @@ exports.getCheckout = catchAsync(async (req, res, next) => {
 			{
 				name: `${tour.name} Tour`,
 				description: tour.summary,
-				images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+				images: [
+					`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`
+				],
 				amount: tour.price * 100,
 				currency: 'usd',
 				quantity: 1
@@ -34,7 +36,7 @@ exports.getCheckout = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async session => {
 	const tour = session.client_reference_id;
-	const user = (await User.find({ email: session.customer_email })).id;
+	const user = (await User.findOne({ email: session.customer_email })).id;
 	const price = session.display_items[0].amount / 100;
 
 	await Booking.create({ tour, user, price });
